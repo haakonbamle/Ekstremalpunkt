c53afd251036e82ae60beea1215a24b50e1802b0
haakonbamle <haakon.siegwarth@gmail.com>
2024-03-19 15:54:31 +0100

    1.

---
+++ src/EkstremalpunktTing.java
@@ -0,0 +1,14 @@
+public class EkstremalpunktTing {
+
+
+    public static void main(String[] args) {
+
+        mohammad mohammad = new mohammad();
+
+        mohammad.print("Hva heter du?");
+        String uwu = mohammad.input();
+        mohammad.print("Hei, " + uwu + "!");
+
+    }
+
+}
---
+++ src/mohammad.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class mohammad {
+
+    public void print(String arg) {
+        System.out.println(arg);
+
+    }
+
+    public String input() {
+        Scanner scanner = new Scanner(System.in);
+        return (scanner.nextLine());
+    }
+    public mohammad() {
+
+
+    }
+
+}
