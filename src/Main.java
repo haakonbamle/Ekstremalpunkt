f9a816327e27d0618c34ea2269b8a835f851e078
haakonbamle <haakon.siegwarth@gmail.com>
2024-03-25 22:40:30 +0100

    1.dONE

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,15 @@ public class Main {
 
             if (Math.round(lib.derivert(i, 0.0001)) == 0) {
                 ekstremalpunkt.add("(" + i + ", " + lib.f(i) + ")");
-                System.out.println("uwu");
                 return;
             }
             else if (i == 100 && ekstremalpunkt.size() == 0) {
                 i = -range;
                 stigning /= 10;
-                lib.print("Tom");
             } else if (stigning == 0.0001) {
-                lib.print("Stigning er 0.0001");
                 range *= 10;
                 stigning = s;
             } else if (range > 10000) {
-                lib.print("Ut av range");
                 return;
             }
 
